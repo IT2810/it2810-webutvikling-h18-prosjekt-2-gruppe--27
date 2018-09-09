f8f4f45a2ad07c60ea16505f0f344b30619577a7
Artorp <Artorp@users.noreply.github.com>
2018-09-10 00:54:00 +0200

    Add basic functionality for permutation

--- src/components/Art/Art.js
+++ src/components/Art/Art.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
 class Art extends Component {
+
   render() {
     return (
       <div>
-        <img src={this.props.img} alt={this.props.img} />
+        <img src={this.props.imageSrc} alt={"This is the category"}
+         style={{maxWidth: "400px", maxHeight: "400px"}}
+        />
         {/*TODO: Add div for text and for music???*/}
       </div>
     );
