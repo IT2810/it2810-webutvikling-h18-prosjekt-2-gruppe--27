b7af8fbe348d6aa7b237f7eae8eac72da15b0bf2
Artorp <Artorp@users.noreply.github.com>
2018-09-09 21:23:08 +0200

    Deep copy resources

--- src/App.js
+++ src/App.js
@@ -3,8 +3,47 @@ import logo from './logo.svg';
 import './App.css';
 import AjaxCall from "./AjaxCall.js";
 import Art from "./components/Art/Art";
+import importedResources from "./resources";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    const state = {};
+
+    // generate objects for each static resource
+    const resources = {};
+    for (let fileType in importedResources) {
+      resources[fileType] = {};
+      for (let cat in importedResources[fileType]) {
+        if (importedResources[fileType].hasOwnProperty(cat)) {
+          resources[fileType][cat] = [];
+          importedResources[fileType][cat].forEach((e, i) => {
+            resources[fileType][cat].push({
+              id: i,
+              url: `${fileType}/${cat}/${e}`,
+              loaded: false,
+              error: false,
+              blob: null
+            });
+          });
+        }
+      }
+    }
+    state.resources = resources;
+    console.log(resources);
+
+    state.activeTab = 0;
+    state.activeCategory = {image: "", sound: "", text: ""};
+    state.permutation = {
+      imageId: [],
+      soundId: [],
+      textId: []
+    };
+
+    this.state = state;
+  }
+
+
   render() {
     return (
       <div className="App">
