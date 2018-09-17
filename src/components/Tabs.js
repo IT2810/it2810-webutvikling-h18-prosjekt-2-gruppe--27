08751e3538802f3a7c74ced44f12106289d3ac90
Sander Breivik <sandeb@stud.ntnu.no>
2018-09-17 23:24:47 +0200

    Changed colors and design clean up

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import Tab from './Tab';
 
-class Tabs extends Component {
+class Navbar extends Component {
   render() {
     return (
-      <div  className={this.props.className}>
+      <div className={this.props.className}>
+        <h1>{this.props.title}</h1>
         <ol className="tab-list">
           {this.props.children.map((child, index) => {
             const name = child.props.children;
@@ -24,4 +25,4 @@ class Tabs extends Component {
   }
 }
 
-export default Tabs;
+export default Navbar;
