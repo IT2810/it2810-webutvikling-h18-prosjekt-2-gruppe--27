b15870c16b6c20ab7831ba688cc9c1e5d4c91032
Artorp <Artorp@users.noreply.github.com>
2018-09-18 11:20:16 +0200

    Add media text for sound
    As part of https://github.com/IT2810/it2810-webutvikling-h18-prosjekt-2-gruppe--27/issues/9

--- src/components/Art/SoundView.js
+++ src/components/Art/SoundView.js
@@ -3,12 +3,15 @@ import React, {Component} from 'react';
 class SoundView extends Component {
   render() {
     let src = "";
+    let info_text = "No sound selected";
     const sound = this.props.sound;
     if (sound && sound.state === "loaded") {
       src = URL.createObjectURL(sound.data);
+      info_text = sound.filename;
     }
     return (
       <div className={this.props.className}>
+        <p>Now playing: {info_text}</p>
         <audio src={src} controls autoPlay loop/>
       </div>
     );
